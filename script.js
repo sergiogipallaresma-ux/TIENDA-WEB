dc43cc13c8ee41db9b05f1666571e78925fd6de4
sergiogipallaresma-ux <sergiogi.pallaresma@ecci.edu.co>
2026-03-21 00:04:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const sliders = document.querySelectorAll(".slides");
+let indexSlides = [];
+
+// Inicializar índices
+sliders.forEach((_, i) => {
+    indexSlides[i] = 0;
+});
+
+function moverSlide(direccion, producto) {
+
+    const slider = sliders[producto];
+    const total = slider.children.length;
+
+    indexSlides[producto] += direccion;
+
+    if (indexSlides[producto] < 0) {
+        indexSlides[producto] = total - 1;
+    }
+
+    if (indexSlides[producto] >= total) {
+        indexSlides[producto] = 0;
+    }
+
+    slider.style.transform = `translateX(-${indexSlides[producto] * 100}%)`;
+}
+
 function toggleWhatsApp() {
     const menu = document.getElementById("whatsappOptions");
 
@@ -6,4 +32,23 @@ function toggleWhatsApp() {
     } else {
         menu.style.display = "flex";
     }
+}
+
+let productoSeleccionado = "";
+let precioSeleccionado = "";
+
+// Esta función la llamas desde cada producto
+function seleccionarProducto(nombre, precio) {
+    productoSeleccionado = nombre;
+    precioSeleccionado = precio;
+}
+
+// Esta función se ejecuta al elegir el número
+function enviarWhatsApp(numero) {
+
+    let mensaje = `Hola, quiero comprar (inserte nombre del producto y captura de pantalla). ¿Sigue disponible?`;
+
+    let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
+
+    window.open(url, "_blank");
 }
